ef186e77352511bc6c57b0555e34f057c928ed39
Hejo <59996938+codezay@users.noreply.github.com>
2023-02-01 17:53:45 +0530

    form validation

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  // const RangeSelectorPage({super.key, required this.title});
+  const RangeSelectorPage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  final formKey = GlobalKey<FormState>();
+  // ignore: unused_field
+  int _min = 0;
+  // ignore: unused_field
+  int _max = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Range'),
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RangeSelectorTextFormField(
+                labelText: 'Minimum',
+                // info: Lifitin state up.
+                intValueSetter: (value) => _min = value,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              RangeSelectorTextFormField(
+                labelText: 'Maximum',
+                // info: Lifitin state up.
+                intValueSetter: (value) => _max = value,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_forward),
+        onPressed: () {
+          if (formKey.currentState?.validate() == true) {
+            formKey.currentState?.save();
+          }
+          // TODO: Navigate to the generated page
+        },
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  const RangeSelectorTextFormField({
+    Key? key,
+    required this.labelText,
+    required this.intValueSetter,
+  }) : super(key: key);
+
+  final String labelText;
+
+  final void Function(int value) intValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: labelText,
+      ),
+      keyboardType: const TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+      validator: (value) {
+        if (value == null || int.tryParse(value) == null) {
+          return 'This must be an intiger';
+        } else {
+          return null;
+        }
+      },
+      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '')),
+    );
+  }
+}
